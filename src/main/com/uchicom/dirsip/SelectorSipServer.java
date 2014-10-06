fba2029c32bc2a13e807471e1f59530ba821ddd0
uchicom <support@uchicom.com>
2014-10-06 19:20:01 +0900

    フォーマッタ実行

--- src/main/com/uchicom/dirsip/SelectorSipServer.java
+++ src/main/com/uchicom/dirsip/SelectorSipServer.java
@@ -16,107 +16,114 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Channelを利用したSIPサーバー
+ * 
  * @author uchicom: Shigeki Uchiyama
- *
+ * 
  */
 public class SelectorSipServer {
 
-    protected static Queue<ServerSocketChannel> serverQueue = new ConcurrentLinkedQueue<ServerSocketChannel>();
-    
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        SipParameter parameter = new SipParameter(args);
-        if (parameter.init(System.err)) {
-            SelectorSipServer server = new SelectorSipServer(parameter);
-            server.execute();
-        }
-    }
-    private static boolean alive = true;
-    private SipParameter parameter;
-    public SelectorSipServer(SipParameter parameter) {
-	this.parameter = parameter;
-    }
-    /** メイン処理
-     * 
-     */
-    public void execute() {
-        ServerSocketChannel server = null;
-        try {
-            server = ServerSocketChannel.open();
-            server.socket().setReuseAddress(true);
-            server.socket().bind(new InetSocketAddress(parameter.getPort()), parameter.getBack());
-            server.configureBlocking(false);
-            serverQueue.add(server);
-            
-            final Selector selector = Selector.open();
-            server.register(selector, SelectionKey.OP_ACCEPT , new AcceptHandler());
-            Thread thread = new Thread() {
-            	public void run() {
-        			try {
-	            		while (true) {
-	        				System.out.println("");
-	        				System.out.println("keys.size():" + selector.keys().size());
-	            			System.out.println("");
+	protected static Queue<ServerSocketChannel> serverQueue = new ConcurrentLinkedQueue<ServerSocketChannel>();
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SipParameter parameter = new SipParameter(args);
+		if (parameter.init(System.err)) {
+			SelectorSipServer server = new SelectorSipServer(parameter);
+			server.execute();
+		}
+	}
+
+	private static boolean alive = true;
+	private SipParameter parameter;
+
+	public SelectorSipServer(SipParameter parameter) {
+		this.parameter = parameter;
+	}
+
+	/**
+	 * メイン処理
+	 * 
+	 */
+	public void execute() {
+		ServerSocketChannel server = null;
+		try {
+			server = ServerSocketChannel.open();
+			server.socket().setReuseAddress(true);
+			server.socket().bind(new InetSocketAddress(parameter.getPort()),
+					parameter.getBack());
+			server.configureBlocking(false);
+			serverQueue.add(server);
+
+			final Selector selector = Selector.open();
+			server.register(selector, SelectionKey.OP_ACCEPT,
+					new AcceptHandler());
+			Thread thread = new Thread() {
+				public void run() {
+					try {
+						while (true) {
+							System.out.println("");
+							System.out.println("keys.size():"
+									+ selector.keys().size());
+							System.out.println("");
 							Thread.sleep(1000);
-	            		}
-            		} catch (InterruptedException e) {
+						}
+					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-            	}
-            };
-            thread.setDaemon(true);
-            thread.start();
-            while (alive) {
-                if (selector.select() > 0) {
-                    Set<SelectionKey> keys = selector.selectedKeys();
-                    Iterator<SelectionKey> ite = keys.iterator();
-                    while(ite.hasNext()) {
-                        SelectionKey key = ite.next();
-                        ite.remove();
-                        try {
-                            if (key.isValid()) {
-                                ((Handler)key.attachment()).handle(key);
-                            } else {
-                                key.cancel();
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                            key.cancel();
-                        } catch (NoSuchAlgorithmException e) {
-                            e.printStackTrace();
-                            key.cancel();
-                        }
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-	        synchronized (server) {
-	            if (server != null) {
-	                try {
-	                    server.close();
-	                } catch (Exception e) {
-	                    e.printStackTrace();
-	                }
-	                server = null;
-	            }
-	        }
-        }
-    }
+				}
+			};
+			thread.setDaemon(true);
+			thread.start();
+			while (alive) {
+				if (selector.select() > 0) {
+					Set<SelectionKey> keys = selector.selectedKeys();
+					Iterator<SelectionKey> ite = keys.iterator();
+					while (ite.hasNext()) {
+						SelectionKey key = ite.next();
+						ite.remove();
+						try {
+							if (key.isValid()) {
+								((Handler) key.attachment()).handle(key);
+							} else {
+								key.cancel();
+							}
+						} catch (IOException e) {
+							e.printStackTrace();
+							key.cancel();
+						} catch (NoSuchAlgorithmException e) {
+							e.printStackTrace();
+							key.cancel();
+						}
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			synchronized (server) {
+				if (server != null) {
+					try {
+						server.close();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					server = null;
+				}
+			}
+		}
+	}
 
-    
-    public static void shutdown(String[] args) {
-        if (!serverQueue.isEmpty()) {
-            try {
-                serverQueue.poll().close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	public static void shutdown(String[] args) {
+		if (!serverQueue.isEmpty()) {
+			try {
+				serverQueue.poll().close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
